32ac93cc7d024759ce7a1f130722c149f8d0a954
tuananh123488 <100123790+tuananh123488@users.noreply.github.com>
2025-10-28 00:25:41 +0700

    Change text content in index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = 'Thanh Trà nhấp vào đi'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
